f1093de01e8e040549847247589b1f813c82025c
Aashish Joshi <jaashish109@gmail.com>
2025-01-17 20:27:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Function to open the resume in full-screen view
+function openResume() {
+    document.getElementById('resumeFullScreen').style.display = 'block';
+}
+
+// Function to close the resume view
+function closeResume() {
+    document.getElementById('resumeFullScreen').style.display = 'none';
+}
